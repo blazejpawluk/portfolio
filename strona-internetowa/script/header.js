ce6f8e83b8ffe3a936adec187f6ee8337437d08f
blazejpawluk <bpawluk05@gmail.com>
2025-08-26 09:48:00 +0200

    different size screen support

--- strona-internetowa/script/header.js
+++ strona-internetowa/script/header.js
@@ -2,6 +2,10 @@ const email = document.getElementById('email');
 const copyEmail = document.getElementById('copy-msg-email');
 const phone = document.getElementById('phone');
 const copyPhone = document.getElementById('copy-msg-phone');
+const menuIcon = document.getElementById('menu');
+const cancelIcon = document.getElementById('cancel');
+const hr1 = document.getElementById('hr1');
+const hiddenMenu = document.getElementById('hidden-menu');
 
 email.addEventListener('click', async (e) => {
 	try {
@@ -29,4 +33,18 @@ phone.addEventListener('click', async (e) => {
 			copyPhone.style.display = 'none';
 		}, 3000);
 	} catch (error) {console.error('Błąd kopiowania:', err);}
+});
+
+menuIcon.addEventListener('click', async (e) => {
+	menuIcon.style.display = "none";
+	cancelIcon.style.display = "block";
+	hr1.style.display = "block";
+	hiddenMenu.style.display = "flex";
+});
+
+cancelIcon.addEventListener('click', async (e) => {
+	menuIcon.style.display = "block";
+	cancelIcon.style.display = "none";
+	hr1.style.display = "none";
+	hiddenMenu.style.display = "none";
 });
